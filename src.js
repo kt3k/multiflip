6e52b710e269d170444965a6b24433f5a70ad545
Yoshiya Hinosawa <stibium121@gmail.com>
2017-02-25 00:10:04 +0900

    :hammer: refactor: use global style and simplify mechanism

--- src.js
+++ src.js
@@ -1,13 +1,13 @@
 const $ = jQuery
 
 const DEFAULT_UNIT_DIR = 400
-const DEFAULT_CONTENT_SHOW_DUR = 400
 
 const DEFAULT_M = 4
 const DEFAULT_N = 4
 
 const DEFAULT_BGCOLOR = '#393F44'
 const CHIP_CLASS = 'multiflip-chip'
+const FLIPPED_CLASS = 'multiflip-flipped'
 
 const FLIP_TRANSFORM = 'rotate3d(1, -1, 0, -180deg)' // Transformation for the flipping a chip
 
@@ -21,7 +21,6 @@ const wait = n => new Promise(resolve => setTimeout(resolve, n))
  * - attr {number} m The horizontal partition number
  * - attr {number} n The vertical partition number
  * - attr {number} unit-dur The unit duration of multiflipping
- * - attr {number} content-show-dur The duration of showing and hiding the content
  * - attr {string} bgcolor The background color of the flipping chips
  */
 export default class Multiflip {
@@ -43,16 +42,16 @@ export default class Multiflip {
     this.unitDur = +this.el.getAttribute('unit-dur') || DEFAULT_UNIT_DIR
     this.diffDur = this.unitDur / (this.m + this.n)
 
-    this.contentShowDur = +this.el.getAttribute('content-show-dur') || DEFAULT_CONTENT_SHOW_DUR
-
     this.bgcolor = this.el.getAttribute('bgcolor') || DEFAULT_BGCOLOR
 
-    this.forEachChild(child => {
-      child.style.opacity = '0'
-      child.style.transitionDuration = this.contentShowDur + 'ms'
+    Multiflip.insertGlobalStyle()
+
+    Array.prototype.forEach.call(this.el.children, child => {
+      child.style.transitionDuration = `${this.unitDur}ms`
+      child.style.transitionDelay = `${this.unitDur / 2}ms`
     })
 
-    this.chips = Array(this.n * this.m).fill().map((_, c) => {
+    Array(this.n * this.m).fill().map((_, c) => {
       const i = c % this.m
       const j = Math.floor(c / this.m)
       const chip = Multiflip.createChip(
@@ -67,9 +66,37 @@ export default class Multiflip {
 
       this.el.insertBefore(chip, this.el.firstChild)
       chip.classList.add(CHIP_CLASS)
-
-      return chip
     })
+  }
+
+  static insertGlobalStyle () {
+    if (document.getElementById('multiflip-global-style')) {
+      return
+    }
+
+    const style = document.createElement('style')
+    style.setAttribute('id', 'multiflip-global-style')
+
+    style.textContent = `
+      .multiflip .${CHIP_CLASS} {
+        position: absolute;
+        transform: ${FLIP_TRANSFORM};
+        backface-visibility: hidden;
+        transform-style: preserve-3d;
+      }
+      .multiflip.${FLIPPED_CLASS} .${CHIP_CLASS} {
+        transform: none;
+      }
+      .multiflip :not(.${CHIP_CLASS}) {
+        opacity: 0;
+        transition-property: opacity;
+      }
+      .multiflip.${FLIPPED_CLASS} > :not(.${CHIP_CLASS}) {
+        opacity: 1;
+      }
+    `
+
+    document.body.appendChild(style)
   }
 
   /**
@@ -84,7 +111,6 @@ export default class Multiflip {
     const div = document.createElement('div')
     const style = div.style
 
-    style.position = 'absolute'
     style.left = left + 'px'
     style.top = top + 'px'
     style.width = w + 'px'
@@ -92,9 +118,6 @@ export default class Multiflip {
     style.backgroundColor = bgcolor
     style.transitionDuration = unitDur + 'ms'
     style.transitionDelay = delay + 'ms'
-    style.transform = FLIP_TRANSFORM
-    style.backfaceVisibility = 'hidden'
-    style.transformStyle = 'preserve-3d'
 
     return div
   }
@@ -104,20 +127,9 @@ export default class Multiflip {
    * @return {Promise}
    */
   show () {
-    this.chips.forEach(chip => { chip.style.transform = '' })
+    this.el.classList.add(FLIPPED_CLASS)
 
-    return wait(this.unitDur * 2 - this.contentShowDur).then(() => this.showContents())
-  }
-
-  /**
-   * Shows the contents.
-   * @private
-   * @return {Promise}
-   */
-  showContents () {
-    this.forEachChild(child => { child.style.opacity = '1' })
-
-    return wait(this.contentShowDur) // waits for the content showing
+    return wait(this.unitDur * 2)
   }
 
   /**
@@ -125,30 +137,8 @@ export default class Multiflip {
    * @return {Promise}
    */
   hide () {
-    return this.hideContents()
-      .then(() => {
-        this.chips.forEach(chip => { chip.style.transform = FLIP_TRANSFORM })
+    this.el.classList.remove(FLIPPED_CLASS)
 
-        return wait(this.unitDur * 2)
-      })
-  }
-
-  /**
-   * Hides the contents.
-   * @private
-   * @return {Promise}
-   */
-  hideContents () {
-    this.forEachChild(child => { child.style.opacity = '0' })
-
-    return wait(this.contentShowDur)
-  }
-
-  /**
-   * Calls the given func on each child.
-   * @param {Function} func The function
-   */
-  forEachChild (func) {
-    Array.prototype.forEach.call(this.el.querySelectorAll(`:not(.${CHIP_CLASS})`), func)
+    return wait(this.unitDur * 2)
   }
 }
